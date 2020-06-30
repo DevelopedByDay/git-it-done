d6879988060c472c6648ceb037d163d1ecfc7d57
BradD <bradleyeday@gmail.com>
2020-06-30 13:58:44 -0500

    added form with username input

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,14 +1,30 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+
+function formSubmitHandler(event) {
+    event.preventDefault();
+    
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    }
+    else {
+        alert("Please enter a GitHub username");
+    }
+}
+
 function getUserRepos(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
-            console.log(data);
         });
     })
-    
+
 };
 
-console.log("outside");
-
 getUserRepos();
+userFormEl.addEventListener("submit", formSubmitHandler);
